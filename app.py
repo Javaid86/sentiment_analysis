fd8a6011504f518c739c37afef0f2eb85cb3fac6
Javaid86 <97302408+Javaid86@users.noreply.github.com>
2023-12-07 20:13:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,51 +15,51 @@ import nltk
 nltk.download('stopwords')
 english_stops = set(stopwords.words('english'))
 #reading the dataset
-data=pd.read_csv('IMDB Dataset.csv')
+# data=pd.read_csv('IMDB Dataset.csv')
 
-def load_dataset():
-    data = pd.read_csv('IMDB Dataset.csv')
-    x_data = data['review']       # Reviews/Input
-    y_data = data['sentiment']    # Sentiment/Output
+# def load_dataset():
+#     data = pd.read_csv('IMDB Dataset.csv')
+#     x_data = data['review']       # Reviews/Input
+#     y_data = data['sentiment']    # Sentiment/Output
 
-    # PRE-PROCESS REVIEW
-    x_data = x_data.replace({'<.*?>': ''}, regex = True)          # remove html tag
-    x_data = x_data.replace({'[^A-Za-z]': ' '}, regex = True)     # remove non alphabet
-    x_data = x_data.apply(lambda review: [w for w in review.split() if w not in english_stops])  # remove stop words
-    x_data = x_data.apply(lambda review: [w.lower() for w in review])   # lower case
+#     # PRE-PROCESS REVIEW
+#     x_data = x_data.replace({'<.*?>': ''}, regex = True)          # remove html tag
+#     x_data = x_data.replace({'[^A-Za-z]': ' '}, regex = True)     # remove non alphabet
+#     x_data = x_data.apply(lambda review: [w for w in review.split() if w not in english_stops])  # remove stop words
+#     x_data = x_data.apply(lambda review: [w.lower() for w in review])   # lower case
 
-    # ENCODE SENTIMENT -> 0 & 1
-    y_data = y_data.replace('positive', 1)
-    y_data = y_data.replace('negative', 0)
+#     # ENCODE SENTIMENT -> 0 & 1
+#     y_data = y_data.replace('positive', 1)
+#     y_data = y_data.replace('negative', 0)
 
-    return x_data, y_data
+#     return x_data, y_data
 
-x_data, y_data = load_dataset()
+# x_data, y_data = load_dataset()
 
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.2)
+# x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.2)
 
-def get_max_length():
-    review_length = []
-    for review in x_train:
-        review_length.append(len(review))
+# def get_max_length():
+#     review_length = []
+#     for review in x_train:
+#         review_length.append(len(review))
 
-    return int(np.ceil(np.mean(review_length)))
-token = Tokenizer(lower=True)
-token.fit_on_texts(x_train)
-x_train = token.texts_to_sequences(x_train)
-x_test = token.texts_to_sequences(x_test)
+#     return int(np.ceil(np.mean(review_length)))
+# token = Tokenizer(lower=True)
+# token.fit_on_texts(x_train)
+# x_train = token.texts_to_sequences(x_train)
+# x_test = token.texts_to_sequences(x_test)
 
-max_length = get_max_length()
+# max_length = get_max_length()
 
-x_train = pad_sequences(x_train, maxlen=max_length, padding='post', truncating='post')
-x_test = pad_sequences(x_test, maxlen=max_length, padding='post', truncating='post')
+# x_train = pad_sequences(x_train, maxlen=max_length, padding='post', truncating='post')
+# x_test = pad_sequences(x_test, maxlen=max_length, padding='post', truncating='post')
 
-total_words = len(token.word_index) + 1   # add 1 because of 0 padding
+# total_words = len(token.word_index) + 1   # add 1 because of 0 padding
 
-import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import streamlit as st
+# import tensorflow as tf
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Load the pre-trained model
 loaded_model = load_model('Sentiment_Analysis_model.h5')
